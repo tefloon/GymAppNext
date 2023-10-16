d6bfa221f236d2bcafeaefcd0925cae98ca961b6
tefloon <tefloon@gmail.com>
2023-10-16 14:27:31 +0200

    Initial commit. Tailwind + NextUI + next-themes.

---
+++ src/app/about/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import CustomCard from "@/components/CustomCard";
+import React from "react";
+
+export default function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <CustomCard />
+    </div>
+  );
+}
---
+++ src/app/providers.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { NextUIProvider } from "@nextui-org/react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import React from "react";
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <NextUIProvider>
+      <NextThemesProvider
+        attribute="class"
+        defaultTheme="dark"
+        themes={["light", "dark", "modern"]}
+      >
+        {children}
+      </NextThemesProvider>
+    </NextUIProvider>
+  );
+}
